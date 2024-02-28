6ac144600b3044f12ffbab37d958bf0b4a359a25
eatthecookies <Alexander.Vorobjev1@ya.ru>
2024-02-28 20:22:52 +0300

    Добавил тесты для своей реализации ExecutirService

---
+++ src/lab4/TestClass.java
@@ -0,0 +1,22 @@
+package lab4;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestClass {
+    public static void main(String[] args) {
+        ExecutorService executor = new MyExecutorService(2);
+        // если использовать более двух потоков, эта задача будет медленнее, потому что тут есть задержка
+        executor.submit(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("We run it");
+        });
+
+        executor.submit(() -> System.out.println("Start"));
+    }
+}
+
