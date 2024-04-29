15e56d94f7b7f7482088364a34fb8ca225bc9482
alexander <79151974+eatthecookies@users.noreply.github.com>
2024-04-29 20:45:16 +0300

    Добавил 11 практику

---
+++ src/lab11/src/main/java/ru/mirea3/java/labs/lab11/Lab11Application.java
@@ -0,0 +1,13 @@
+package ru.mirea3.java.labs.lab11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab11Application.class, args);
+	}
+
+}
---
+++ src/lab11/src/test/java/ru/mirea3/java/labs/lab11/Lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea3.java.labs.lab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
